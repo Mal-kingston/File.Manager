8c114b94f6460f92dc0911ba939866669a756c52
Malachy Okwute <47065240+Mal-kingston@users.noreply.github.com>
2024-02-20 12:18:32 -0500

    window control button on hover fix
    fixed the hover behavior of window control buttons [minimize maximize and close button].

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace File.Manager
 {
@@ -9,12 +7,19 @@ namespace File.Manager
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Manually start this application 
+        /// </summary>
+        /// <param name="e">Start up event </param>
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
+            // Create main window and set it's data context
             var mainAppWindow = new MainWindow { DataContext = new MainWindowViewModel() };
+            // Set app main window to main-app-window 
             Current.MainWindow = mainAppWindow;
+            // Show window
             Current.MainWindow.Show();
 
         }
